3f6398dbf9814f1e2d3ad9ac914d520fa0b1ac65
arshu147 <arshad.thotlipalli@gmail.com>
2018-12-26 17:03:02 +0530

    ggg

--- kirkiri/src/kirkiri/kirkirione.java
+++ kirkiri/src/kirkiri/kirkirione.java
@@ -1,17 +1,16 @@
 package kirkiri;
 
+import org.testng.annotations.Test;
+
 //import org.testng.annotations.Test;
 
 public class kirkirione {
-//	@Test
-//	public void teesst() {
-//		
-//		System.out.println("jenkins workin");
-//		
-//	}
-	
-	public static void main(String[] args) {
-		System.out.println("HIIII");
+	@Test
+	public void teesst() {
+		
+		System.out.println("jenkins workin");
+		
 	}
-
+	
+	
 }
